44691feabcc0d28f3cfd39ae6516ceee4eba16ca
league-workshopper <workshop@Administrators-Mac-mini.local>
2015-07-27 14:54:05 -0700

    commit made a crazy cyan pentagon

--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -9,27 +9,41 @@ import org.jointheleague.graphical.robot.Robot;
 public class PentagonCrazy {
 
 	private void makePrettyThings() {
-		// 1. Create a new Robot
+		
+		Robot Steve= new Robot();
 
-		// 3. Put the robot's pen down
+		Steve.setPenWidth(10);
+		Steve.penDown();
 
-		// 8. Make the robot go at maximum speed (10)
+		
+		Steve.setSpeed(10);
 
-		// 9. Set the pen to a color that you like for the shape
+		
+		Steve.setPenColor(Color.CYAN);
 
-		// 4. Make a variable for the number of sides you want (can’t test this one)
+		
+		int Unicorns= 5;
 
-		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
+		
+        int Puppies= 360/5;
+		
+        for (int i = 0; i < 200; i++) {
+			
+		
 
-		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
-
-			// 2. Move the robot 200 pixels
+			
+				Steve.move(i);
 	
-			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
+			
+				
 	
-			// 6. Turn the robot the amount in your angle variable
+			
+				Steve.turn(Puppies);
 	
-			// 11. Turn the robot one more degree
+			
+				Steve.turn(1);
+				}
+				
 
 	}
 
